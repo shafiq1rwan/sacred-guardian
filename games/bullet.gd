b4711fd495228b21363adf4ed433aaa9338c6d02
MPSB\shafiq.irwan <shafiq.irwan@quickash.com>
2024-08-30 16:26:22 +0800

    Add volume slider

--- games/bullet.gd
+++ games/bullet.gd
@@ -14,11 +14,3 @@ func _physics_process(delta):
 
 func _on_body_entered(_body):
 	queue_free()
-	
-	#const IMPACT = preload("res://assets/pistol/impact/impact.tscn")
-	#var impact = IMPACT.instantiate()
-	#get_parent().add_child(impact)
-	#impact.global_position = global_position
-	#
-	#if body.has_method("take_damage"):
-		#body.take_damage()
--- games/settings.gd
+++ games/settings.gd
@@ -1,4 +1,12 @@
 extends Control
+@export var bus_name: String
+var bgm = AudioServer.get_bus_index("BGM")
 
+func _ready():
+	%VolumeSlider.value = db_to_linear(bgm)
+	
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://games/main_menu.tscn")
+
+func _on_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(bgm, linear_to_db(value))
