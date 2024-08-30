8392b1c91cf8847fe68e391bc011865d544854a4
MPSB\shafiq.irwan <shafiq.irwan@quickash.com>
2024-08-30 12:14:18 +0800

    Add bgm on main menu

--- games/lobby.gd
+++ games/lobby.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://games/game.tscn")
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://games/main_menu.tscn")
--- games/main_menu.gd
+++ games/main_menu.gd
@@ -3,5 +3,5 @@ extends Control
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://games/lobby.tscn")
 
-func _on_exit_button_pressed():
-	get_tree().quit()
+func _on_setting_button_pressed():
+	get_tree().change_scene_to_file("res://games/settings.tscn")
