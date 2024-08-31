9380f26fe5d79e8976d3d442e9970e15b33e45ef
shafiq1rwan <shafiq1rwan00@gmail.com>
2024-08-31 21:21:20 +0800

    Add Wave Level and Score

--- games/game.gd
+++ games/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @onready var pause_menu = %PauseMenu
+@onready var wave_label = $Player/HUD/WaveLabel
+@onready var score_label = $Player/HUD/ScoreLabel
 
 func _on_backto_lobby_pressed():
 	get_tree().change_scene_to_file("res://games/lobby.tscn")
@@ -9,7 +11,10 @@ func _process(_delta):
 		pause_menu.visible = true
 		Engine.time_scale = 0
 		GameManager.IS_PAUSED = true
-
+	
+	wave_label.text = "Wave Level: " + str(GameManager.CURRENT_WAVE_LEVEL)
+	score_label.text = "Score: " + str(GameManager.SCORE)
+	
 func _on_resume_button_pressed():
 	pause_menu.visible = false
 	Engine.time_scale = 1
@@ -21,6 +26,12 @@ func _on_lobby_button_pressed():
 func _on_timer_timeout():
 	if GameManager.TOTAL_MOB_ONSTAGE <= GameManager.TOTAL_MINIMUM_MOB:
 		spawnMob()
+	
+	if GameManager.TOTAL_DEFEATED_MONSTER == GameManager.TOTAL_MOB_ONSTAGE:
+		GameManager.CURRENT_WAVE_LEVEL += 1
+		GameManager.TOTAL_MOB_ONSTAGE = 0
+		GameManager.TOTAL_DEFEATED_MONSTER = 0
+		GameManager.TOTAL_MINIMUM_MOB += 2
 
 func spawnMob():
 	var spawnMOB = preload("res://games/skull_blue.tscn").instantiate()
--- games/skull_blue.gd
+++ games/skull_blue.gd
@@ -2,28 +2,19 @@ extends CharacterBody2D
 
 @onready var ancient_tree = $"../AncientTree"
 @onready var hp_skull_blue = $HPSkullBlue
-@export var detection_radius: float = 300.0 # The range within which the enemy detects the player
-@export var shoot_interval: float = 2.0 # Time between shots
-@export var bullet_scene: PackedScene # The bullet scene to shoot
-@export var bullet_speed: float = 400.0 # Speed of the bullet
 @onready var death_sfx = $DeathSFX
 
 signal mob_dead
 
-var SPEED_MOB = 3000.0
 var HP = 5
-var player: Node2D
-var time_since_last_shot: float = 0.0
 
 func _ready():
 	hp_skull_blue.value = HP
-	# Assuming the player is named "Player" in the scene
-	player = get_parent().get_node("Player")
 
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(ancient_tree.global_position)
-	velocity = direction * SPEED_MOB * delta
+	velocity = direction * GameManager.SPEED_MOB * delta
 	move_and_slide()
 
 #func _on_detection_area_body_entered(body):
@@ -43,3 +34,5 @@ func take_damage():
 
 func _on_death_sfx_finished():
 	queue_free()
+	GameManager.TOTAL_DEFEATED_MONSTER += 1
+	GameManager.SCORE += 1
