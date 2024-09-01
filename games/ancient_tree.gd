2d52247fdf125e23f9255ef18bd37a97128d2265
shafiq1rwan <shafiq1rwan00@gmail.com>
2024-09-01 16:29:52 +0800

    Change UI and Setup Ancient Tree HP

--- games/ancient_tree.gd
+++ games/ancient_tree.gd
@@ -1,8 +1,3 @@
 extends StaticBody2D
 
 var HP = 100.00
-@onready var tree_hp = $TreeHP
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	tree_hp.value = HP
--- games/player.gd
+++ games/player.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
+@onready var player_hp: ProgressBar = $HUD/PlayerHP
+@onready var ancient_tree_hp: ProgressBar = $HUD/AncientTreeHP
+
+
+func _ready() -> void:
+	player_hp.value = GameManager.HP_PLAYER
+	ancient_tree_hp.value = GameManager.ANCIENTTREE_HP
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
