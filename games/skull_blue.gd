37849f734dacf3d7815e86a7ae29abb7bda1910c
shafiq1rwan <shafiq1rwan00@gmail.com>
2024-09-02 07:48:58 +0800

    Commit change Staff and bullet

--- games/skull_blue.gd
+++ games/skull_blue.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var ancient_tree = $"../AncientTree"
+@onready var player = $"../Player"
 @onready var hp_skull_blue = $HPSkullBlue
 @onready var death_sfx = $DeathSFX
 
@@ -10,8 +11,8 @@ func _ready():
 	hp_skull_blue.value = HP
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(ancient_tree.global_position)
-	velocity = direction * GameManager.SPEED_MOB * delta
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * GameManager.SPEED_MOB_IMP * delta
 	move_and_slide()
 
 #func _on_detection_area_body_entered(body):
