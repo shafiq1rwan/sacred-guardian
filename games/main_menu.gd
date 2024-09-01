a7944d476c018face493364fdf54e1e8d475d491
shafiq1rwan <shafiq1rwan00@gmail.com>
2024-09-01 11:26:36 +0800

    Upgrade to v4.3

--- games/main_menu.gd
+++ games/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var bgm: AudioStreamPlayer = $BGM
+
+func _ready() -> void:
+	bgm.play()
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://games/game.tscn")
 
